da9e3f4ef155e6fdb822b8882b562c3178076933
boo <booblegum42@gmail.com>
2025-06-26 01:22:28 +0700

    add SnippetForm

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,7 @@
 from django.http import Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from MainApp.models import Snippet
+from MainApp.forms import SnippetForm
 
 
 def index_page(request):
@@ -9,8 +10,21 @@ def index_page(request):
 
 
 def add_snippet_page(request):
-    context = {'pagename': 'Добавление нового сниппета'}
-    return render(request, 'pages/add_snippet.html', context)
+    if request.method == 'GET':
+        form = SnippetForm()
+        return render(request, 'pages/add_snippet.html', {'form': form})
+
+    if request.method == 'POST':
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            lang = form.cleaned_data['lang']
+            code = form.cleaned_data['code']
+
+            Snippet.objects.create(name=name, lang=lang, code=code)
+            return redirect('snippets-list')
+        else:
+            return render(request, 'pages/add_snippet.html', {'form': form})
 
 
 def snippets_page(request):
