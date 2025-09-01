1cb20d229b567a31af5f9e2a211bde9adc423f16
boo <booblegum42@gmail.com>
2025-09-02 01:06:55 +0700

    CBV: user_notifications -> UserNotificationsView

--- MainApp/views_cbv.py
+++ MainApp/views_cbv.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Prefetch
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, View, ListView
 from django.contrib import messages
+from django.contrib import auth
+from django.shortcuts import redirect
 
 from MainApp.forms import SnippetForm, CommentForm
-from MainApp.models import Snippet, Comment
+from MainApp.models import Snippet, Comment, Notification
 
 
 class AddSnippetView(LoginRequiredMixin, CreateView):
@@ -45,3 +47,28 @@ class SnippetDetailView(DetailView):
         context['pagename'] = f'Сниппет: {snippet.name}'
         context["comments_form"] = comments_form
         return context
+
+
+class UserLogoutView(View):
+    def get(self, request):
+        auth.logout(request)
+        return redirect('home')
+
+# CBV
+# 1. Уменьшается дублирование кода
+# 2.
+class UserNotificationsView(LoginRequiredMixin, ListView):
+    model = Notification
+    template_name = 'pages/notifications.html'
+    context_object_name = "notifications"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['pagename'] = 'Мои уведомления'
+        return context
+
+    def get_queryset(self):
+        notifications = Notification.objects.filter(recipient=self.request.user)
+        # Отмечаем все уведомления как прочитанные при переходе на страницу
+        Notification.objects.filter(recipient=self.request.user).update(is_read=True)
+        return notifications
